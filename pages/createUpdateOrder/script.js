621245ca6f92b533c19b543b38afed00e4bda460
rogerch0505@gmail.com <rogerch0505@gmail.com>
2020-08-08 16:00:36 -0300

    fix create order

--- pages/createUpdateOrder/script.js
+++ pages/createUpdateOrder/script.js
@@ -49,6 +49,8 @@ export default {
         deliveryType: '',
         deliveryFee: 0,
         payment: '',
+        rateCard: '',
+        discount: '',
         items: []
       },
       deliveryForm: ['Retirar', 'Entregar'],
