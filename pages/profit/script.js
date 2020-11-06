9a7215451206ae377c0654d6f2317d9b03bb3d85
rogerch0505@gmail.com <rogerch0505@gmail.com>
2020-11-06 11:45:52 -0300

    adding amountPaid

--- pages/profit/script.js
+++ pages/profit/script.js
@@ -1,11 +1,7 @@
-import {
-  loadingOrderAll
-} from '../../services/order'
-import {
-  loadingCost
-} from '../../services/cost'
+import { loadingOrderAll } from "../../services/order";
+import { loadingCost } from "../../services/cost";
 
-import formatValue from '../../mixins/formatValue'
+import formatValue from "../../mixins/formatValue";
 
 export default {
   mixins: [formatValue],
@@ -16,79 +12,87 @@ export default {
       totalCost: 0,
       tableFinanceProfit: [],
       tableRecipeProfit: [],
-      headers: [{
-        text: '',
-        value: 'description'
-      }, {
-        text: '',
-        value: 'total'
-      }]
-    }
+      headers: [
+        {
+          text: "",
+          value: "description"
+        },
+        {
+          text: "",
+          value: "total"
+        }
+      ]
+    };
   },
   methods: {
     async init() {
-      await this.loadCost()
-      await this.loadOrder()
+      await this.loadCost();
+      await this.loadOrder();
 
-      this.tableFinanceProfit = [{
-        description: 'Financeiro',
-        total: this.totalFinance
-      }, {
-        description: 'Custos',
-        total: this.totalCost
-      }]
+      this.tableFinanceProfit = [
+        {
+          description: "Financeiro",
+          total: this.totalFinance
+        },
+        {
+          description: "Custos",
+          total: this.totalCost
+        }
+      ];
 
-      this.tableRecipeProfit = [{
-        description: 'Receita',
-        total: this.totalRecipe
-      }, {
-        description: 'Custos',
-        total: this.totalCost
-      }]
+      this.tableRecipeProfit = [
+        {
+          description: "Receita",
+          total: this.totalRecipe
+        },
+        {
+          description: "Custos",
+          total: this.totalCost
+        }
+      ];
     },
     async loadCost() {
       try {
         await loadingCost().then(res => {
-          this.totalCost = res.data.cost.reduce((acc, {
-            total
-          }) => acc + parseFloat(total), 0)
-        })
+          this.totalCost = res.data.cost.reduce(
+            (acc, { total }) => acc + parseFloat(total),
+            0
+          );
+        });
       } catch (error) {
-        throw error
+        throw error;
       }
     },
     async loadOrder() {
       try {
         await loadingOrderAll().then(res => {
-          this.totalFinance = res.data.order.reduce((acc, {
-            items,
-            deliveryFee,
-            paymentStatus,
-            discount,
-            rateCard,
-            datePayment
-          }) =>  parseFloat(`${ paymentStatus && new Date() >= new Date(datePayment) ? acc + (items.reduce((acc, {
-            amount,
-            value
-          }) => acc + parseFloat(amount * value), 0) + parseFloat(deliveryFee || 0) - (discount || 0)) * ((100-rateCard)/100) : acc + 0 }`), 0)
-          
-          this.totalRecipe = res.data.order.reduce((acc, {
-            items,
-            deliveryFee,
-            discount,
-            rateCard
-          }) =>  parseFloat(acc + (items.reduce((acc, {
-            amount,
-            value
-          }) => acc + parseFloat(amount * value), 0) + parseFloat(deliveryFee || 0) - (discount || 0 )) * ((100-rateCard)/100)), 0)
-          
-        })
+          this.totalFinance = res.data.order.reduce(
+            (acc, { amountPaid }) => acc + amountPaid,
+            0
+          );
+
+          this.totalRecipe = res.data.order.reduce(
+            (acc, { items, deliveryFee, discount, rateCard }) =>
+              parseFloat(
+                acc +
+                  (items.reduce(
+                    (acc, { amount, value }) =>
+                      acc + parseFloat(amount * value),
+                    0
+                  ) +
+                    parseFloat(deliveryFee || 0) -
+                    (discount || 0)) *
+                    ((100 - rateCard) / 100)
+              ),
+            0
+          );
+        });
       } catch (error) {
-        throw error
+        throw error;
       }
     }
   },
   mounted() {
-    this.init()
+    this.init();
   }
-}
+};
